e4aef88f20b3cf77df7066e32f282da71a2eb441
hayem <marc.haye@etu.umontpellier.fr>
2026-03-06 21:32:42 +0100

    Migrate to codeberg

--- go.mod
+++
@@ -1,4 +0,0 @@
-module github.com/doruo/gameoflife
-
-go 1.24.4
-
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/doruo/gameoflife/gol"
-
-func main() {
-	size, lag := 45, 800
-	game := gol.NewGame(size, lag)
-	game.Run()
-}
